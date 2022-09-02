10df96b51f973bb5c3535ac807e100b34ed81eae
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-09-02 21:15:19 +0300

    Task03 done

---
+++ Task03/Program.cs
@@ -0,0 +1,55 @@
+﻿// Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"X({i}) Y({j}) ");
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"Z({k})={array[i, j, k]}; ");
+            }
+            Console.WriteLine("");
+        }
+        Console.WriteLine("");
+    }
+}
+void fillArray(int[,,] array3D)
+{
+    int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+    int num;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        num = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    num = temp[i];
+                }
+                num = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array3D.GetLength(0); x++)
+    {
+        for (int y = 0; y < array3D.GetLength(1); y++)
+        {
+            for (int z = 0; z < array3D.GetLength(2); z++)
+            {
+                array3D[x, y, z] = temp[count]; 
+                count++;
+            }
+        }
+    }
+}
+int[,,] array3D = new int[3, 3, 3];
+fillArray(array3D);
+PrintArray(array3D);
